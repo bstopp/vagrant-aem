8c24be9dfb7628badeb0c92b8c248e6dd5392f12
Bryan Stopp <bryan.stopp@gmail.com>
2018-12-18 11:26:50 -0600

    For some reason this is required or it breaks?

--- setup/update-vm.sh
+++ setup/update-vm.sh
@@ -13,7 +13,7 @@ echo 'Basic VM Provisioning: Updating Ruby gems.'
 yum install -y gcc ruby-devel rubygems
 
 echo 'Basic VM Provisioning: Installing RVM'
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg2 --keyserver hkp://keys.gnupg.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -L get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
 rvm reload
