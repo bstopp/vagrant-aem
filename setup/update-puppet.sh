66e9a26841ef155acb067f7c3580f86579f414af
Bryan Stopp <stopp@adobe.com>
2020-02-07 18:07:08 -0600

    Init issues.
    Update to Puppet 6
    
    Don't install RVM
    
    Librarian puppet tmp can't be mounted path.

--- setup/update-puppet.sh
+++ setup/update-puppet.sh
@@ -4,29 +4,26 @@ echo 'Starting Puppet VM Provisioning'
 
 # Install puppet
 echo 'Puppet VM Provisioning: Installing puppet.'
-rpm -Uvh http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
-yum -y install puppet
+rpm -Uvh https://yum.puppet.com/puppet6-release-el-5.noarch.rpm
+yum -y install puppet-agent
 echo 'PATH=$PATH:/opt/puppetlabs/bin' >> ~/.bash_profile
 echo 'export PATH' >> ~/.bash_profile
 . ~/.bash_profile
 
-gem install librarian-puppet --no-document
+rvm use system
+gem install librarian-puppet
 
 # Install puppet modules.
 echo 'Puppet VM Provisioning: Installing puppet modules.'
 rm -Rf /vagrant/puppet/.librarian
 rm -Rf /vagrant/puppet/.tmp
-if [[ -f /vagrant/puppet/Puppetfile.lock ]]; then
-  rm /vagrant/puppet/Puppetfile.lock
-fi
+rm -Rf /vagrant/puppet/Puppetfile.lock
 
 # Cleaning old modules, just in case.
-if [[ -e /vagrant/puppet/modules ]]; then
-	rm -Rf /vagrant/puppet/modules/*
-fi
+rm -Rf /vagrant/puppet/modules/*
 
 # Install gems for Puppet
 cd /vagrant/puppet
-librarian-puppet install
+LIBRARIAN_PUPPET_TMP=/tmp /usr/local/bin/librarian-puppet install
 
 echo 'Finished basic VM Provisioning.'
--- setup/update-vm.sh
+++ setup/update-vm.sh
@@ -13,15 +13,17 @@ echo 'Basic VM Provisioning: Updating Ruby gems.'
 yum install -y gcc ruby-devel rubygems
 
 echo 'Basic VM Provisioning: Installing RVM'
-gpg2 --keyserver hkp://keys.gnupg.net:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -L get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
-rvm reload
-rvm install 2.5
-rvm use 2.5 --default
-
-
-gem update --system --no-document
-gem uninstall --all
+#gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+#gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+#
+#curl -L get.rvm.io | bash -s stable
+#source /etc/profile.d/rvm.sh
+#rvm reload
+#rvm install 2.5
+#rvm use 2.5 --default
+#
+#
+#gem update --system --no-document
+#gem uninstall --all
 
 echo 'Finished basic VM Provisioning.'
